114f32711bc6d899574d8cb5682075d4802aee99
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 14:57:11 +0000

    Integrated new and create actions for portfolios

--- app/controllers/porfolios_controller.rb
+++ app/controllers/porfolios_controller.rb
@@ -1,2 +1,23 @@
-class PorfoliosController < ApplicationController
+class PortfolioController < ApplicationController
+def idex
+  @portfoilio_items = Porfolio.all
 end
+
+    def new
+        @portfoilio_item = Portfolio.new
+    end
+end
+
+def create
+   @portfoilio_item= Portfolio.new(params.require(:Portfolio).permit(:title, :subtitle, :body))
+  
+    respond_to do |format|
+      if @portfoilio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfoilio_item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+  
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    Title: "My Blog post #{blog}",
+    Body: "hey person"
+  
+  )
+end
+5.times do |skill|
+  Skill.create!(
+    titles: "Rails #{skill}",
+    percent_utilized: 15
+  )  
+end
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title:#{portfolio_item}", 
+    subtitle: "My great service"
+    body: "Hey I love kool stuff",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200",
+    )
+  end  
